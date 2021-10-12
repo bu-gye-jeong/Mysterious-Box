90b84e9ad81f4476b8d1a0cff7d40ae2f9ae54af
bu-gye-jeong <jaey20179@gmail.com>
2021-10-13 01:18:59 +0900

    change arrow function to normal function

--- scripts/data.js
+++ scripts/data.js
@@ -1,5 +1,7 @@
 export const upgradeChance = (l) => 0.9 ** l;
 
 export const upgradeCosts = {
-  boxUpgrade: (l) => Math.floor(1.6 ** l),
+  boxUpgrade(l) {
+    return Math.floor(1.6 ** l);
+  },
 };
--- scripts/setters.js
+++ scripts/setters.js
@@ -1,17 +1,17 @@
 import { upgradeChance, upgradeCosts } from "./data.js";
 
 export const setters = {
-  point: (v) => {
+  point(v) {
     document.getElementById("point").innerHTML = v + " pt";
   },
-  reinforcement: (v) => {
+  reinforcement(v) {
     document.getElementById("boxUpgrade").innerHTML = "+" + v;
     document.getElementById("boxUpgradeChance").innerHTML =
       (upgradeChance(v) * 100).toFixed(1) + "%";
     document.getElementById("boxUpgradeCost").innerHTML =
       "Cost: " + upgradeCosts.boxUpgrade(v);
   },
-  items: (v) => {
+  items(v) {
     [...document.getElementsByClassName("itemCount")].forEach((ele, idx) => {
       ele.innerHTML = v[idx];
     });
